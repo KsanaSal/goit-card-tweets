75a30e143fcd38ab385bd083fa03019aa8627352
KsanaSal <oasalivon@gmail.com>
2023-03-20 23:14:27 +0200

    Added json data and skeleton card

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,11 @@
+import Button from "../button/Button";
+
+const Card = () => {
+    return (
+        <div>
+            Card
+            <Button />
+        </div>
+    );
+};
+export default Card;
