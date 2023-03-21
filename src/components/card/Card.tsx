d0d214832eb8b077b68dfbc14e1d02553877b840
KsanaSal <oasalivon@gmail.com>
2023-03-21 13:38:40 +0200

    Added card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,10 +1,26 @@
 import Button from "../button/Button";
+import css from "./Card.module.css";
+import logo from "../../assets/images/Logo.png";
+import thickQuestion from "../../assets/images/thick_question.png";
 
 const Card = () => {
     return (
-        <div>
-            Card
-            <Button />
+        <div className={css.wrapCard}>
+            <img src={logo} alt="Logo" className={css.logo} />
+            <img
+                src={thickQuestion}
+                alt="Think and question"
+                className={css.image}
+            />
+            <div className={css.line}></div>
+            <div className={css.wrapPhoto}>
+                <img src="" alt="" />
+            </div>
+            <div className={css.wrapText}>
+                <p className={css.text}><span></span>tweets</p>
+                <p className={`${css.text} ${css.followers}`}>followers</p>
+                <Button />
+            </div>
         </div>
     );
 };
